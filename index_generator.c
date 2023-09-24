23713b883c35ac188dd768161bdf2afe8b60e749
Parmenide <parmenin@andrew.cmu.edu>
2023-09-24 09:03:49 +0000

    25

--- index_generator.c
+++ index_generator.c
@@ -3,13 +3,13 @@
 #include <stdlib.h>
 #include <ctype.h>
 #include <string.h>
+#include <stdbool.h>
 #include "common_words.h"
 
-#define BUF_LEN 255
-#define WORD_LEN 20
+#define BUF_LEN 255 // + 1 FOR '\n'
+#define WORD_LEN 21 // + 1 FOR '\0'
 #define WORD_LINE_COUNT 20
 #define UNIQ_WORDS_COUNT 1200
-#define OUTPUT_FILE "./out/out.out"
 
 // WORD STRUCTURE
 typedef struct
@@ -24,7 +24,6 @@ word_t words[UNIQ_WORDS_COUNT];
 
 // OTHER VARIABLES
 int unique_word_count = 0;
-FILE *output;
 
 /*
  * Record a word into the array of words structure
@@ -36,48 +35,59 @@ FILE *output;
  * Returns:
  * None
  */
-void store_words(char *word, int line_number)
+void store_words(char *this_word, int line_number)
 {
-    int exist = 1;
+    bool exist = false;
 
     // CHECKING IF THE WORD ALREADY EXISTS BEFORE STORING IN THE ARRAY
     for (int i = 0; i < UNIQ_WORDS_COUNT; i++)
     {
-        if (strcmp(words[i].wrd, word) == 0)
+        // IF THE WORD EXISTS, INCREASE ITS OCCURENCIES & SET THE FLAG TO TRUE
+        if (strcmp(words[i].wrd, this_word) == 0)
         {
-            // CHECK IF WORD DOESN'T APPEAR ON MORE THAN 20 DISTINCT LINES
-            int lineCount = 0; // Initialize a counter for line numbers
-
-            for (int l = 0; words[i].line_nbrs[l]; l++)
-            {
-                lineCount++; // Increment the line count
-                if (lineCount > 20)
-                {
-                    fprintf(stderr, "The word '%s' is appearing on more than 20 lines\n", word);
-                    exit(1);
-                }
-            }
-
-            // INCREASE OCCURENCIES
             words[i].wrd_occ++;
+            exist = true;
 
-            // ADD NEW LINE OCCURENCE
-            for (int j = 0; j < WORD_LINE_COUNT; j++)
+            // TRACKING THE DISTINCT LINES A WORD CAN APPEAR ON
+            int distinct_lines = 0;
+            for (distinct_lines = 0; distinct_lines < WORD_LINE_COUNT; distinct_lines++)
             {
-                if (words[i].line_nbrs[j] == 0)
+                if (words[i].line_nbrs[distinct_lines] == 0)
                 {
-                    words[i].line_nbrs[j] = line_number;
+                    // CHECKING IF THE LINE NUMBER ALREADY EXISTS BEFORE STORING IN THE ARRAY
+                    int lineExists = 0;
+                    for (int k = 0; k < distinct_lines; k++)
+                    {
+                        if (words[i].line_nbrs[k] == line_number)
+                        {
+                            lineExists = 1;
+                            break; // GO TO THE NEXT WORD
+                        }
+                    }
+
+                    // IF THE LINE NUMBER DOESN'T EXIST, STORE IT
+                    if (!lineExists)
+                        words[i].line_nbrs[distinct_lines] = line_number;
+
+                    // GO TO THE NEXT WORD
                     break;
                 }
             }
-            exist = 0;
+
+            // IF THE WORD APPEARS ON MORE THAN 20 LINES - ERROR
+            if (distinct_lines >= WORD_LINE_COUNT)
+            {
+                fprintf(stderr, "The word '%s' appears lines greater than 20.\n", words[i].wrd);
+                exit(1);
+            }
         }
     }
 
-    if (exist != 0)
+    // IT DOESN'T EXIST
+    if (exist != true)
     {
         // STORING THE WORD AND INITIALIZE OTHER FIELDS
-        strncpy(words[unique_word_count].wrd, word, sizeof(words[unique_word_count].wrd) - 1);
+        strncpy(words[unique_word_count].wrd, this_word, sizeof(words[unique_word_count].wrd) - 1);
         words[unique_word_count].wrd[sizeof(words[unique_word_count].wrd) - 1] = '\0'; // TERMINATING THE WORD
         words[unique_word_count].wrd_occ = 1;
         words[unique_word_count].line_nbrs[0] = line_number;
@@ -87,15 +97,23 @@ void store_words(char *word, int line_number)
     }
 }
 
-int isCommonWord(const char *word)
+/*
+ * Check if a word is a common word - compare with words in common_words.h
+ *
+ * Parameters:
+ * word:            One word to be checked
+ *
+ * Returns:
+ * true if the word is a common word, false otherwise
+ */
+bool isCommonWord(const char *word)
 {
-    int result = 1;
+    bool result = false;
     for (int i = 0; common_words[i] != NULL; i++)
     {
         if (strcmp(word, common_words[i]) == 0)
-            result = 0;
+            result = true;
     }
-
     return result;
 }
 
@@ -104,14 +122,14 @@ int isCommonWord(const char *word)
  *
  * Parameters:
  * ptr_line:        One line of input.
- * word:            One word that has been parsed
- * line_number:     Line number a word is parsed from.
+ * line_number:     Line number a word will be parsed from.
  *
  * Returns:
  * None
  */
-void read_words(char *ptr_line, char *word, int line_number)
+void read_words(char *ptr_line, int line_number)
 {
+    char this_word[WORD_LEN];
     while (*ptr_line != '\0')
     {
         // START AT THE FIRST ADDRESS OF word
@@ -120,30 +138,33 @@ void read_words(char *ptr_line, char *word, int line_number)
         // CHECK EVERY LETTER TO MAKE A WORD
         while (isalpha(*ptr_line) || (*ptr_line == '\'' && isalpha(*(ptr_line + 1)) && isalpha(*(ptr_line - 1))) || (*ptr_line == '-' && isalpha(*(ptr_line + 1)) && isalpha(*(ptr_line - 1))))
         {
-            if (i > 19)
+            if (i >= WORD_LEN - 1)
             {
-                fprintf(stderr, "The word '%s...' is longer than 20 characters!\n", word);
+                // TERMINATE WORD
+                this_word[i] = '\0';
+                fprintf(stderr, "The word '%s...' is longer than 20 characters!\n", this_word);
                 exit(1);
             }
 
             // STORE EACH CHAR FOR CURRENT WORD
-            word[i] = tolower(*ptr_line);
+            this_word[i] = tolower(*ptr_line);
             i++;
             ptr_line++;
         }
 
-        // TERMINATE RESULTED WORD
-        word[i] = '\0';
+        // TERMINATE THE RESULTED WORD
+        this_word[i] = '\0';
 
-        // STORING IN STRUCT ARRAY
-        // IF NOT EXIST, CREATE NEW ENTRY INTO WORDS STRUCT AND INCREMENT THE unique_word_count
-        if (word[0] != '\0' && isCommonWord(word) == 1)
+        // IF THE WORD IS NOT EMPTY AND NOT A COMMON WORD - STORE IT
+        if (this_word[0] != '\0' && isCommonWord(this_word) == false)
         {
+            // IF THE WORD DIDN'T EXCEED 1200 COUNT - STORE IT
             if (unique_word_count < UNIQ_WORDS_COUNT)
             {
-                store_words(word, line_number);
-                // EMPTYING THE WORD BUFFER FOR THE NEXT WORD
-                memset(word, '\0', WORD_LEN);
+                store_words(this_word, line_number);
+
+                // RESET THE WORD TO EMPTY FOR THE NEXT WORD TO BE STORED
+                memset(this_word, '\0', WORD_LEN);
             }
             else
             {
@@ -152,30 +173,40 @@ void read_words(char *ptr_line, char *word, int line_number)
             }
         }
 
-        // ADVANCE THE POINTER TO THE END OF THE LINE
+        // MOVE TO THE NEXT CHAR IN THE LINE TO CHECK FOR THE NEXT WORD
         ptr_line++;
     }
 }
 
 /*
- * Comparison function for qsort to sort words by wrd_occ in descending order
+ * Compare two words by their occurencies and alphabetical order
+ *
+ * Parameters:
+ * a:   Pointer to the first word to be compared
+ * b:   Pointer to the second word to be compared
+ *
+ * Returns:
+ * 0 if the words are equal, negative if the first word is less than the second, positive otherwise
  */
 int compare_words(const void *a, const void *b)
 {
     const word_t *wordA = (const word_t *)a;
     const word_t *wordB = (const word_t *)b;
 
-    // COMPARING WORDS BY OCCURENCE IF NOT EQUAL, AND BY ALPHA ORDER OTHERWISE
+    // COMPARING WORDS BY OCCURENCE IF NOT EQUAL
     if (wordB->wrd_occ != wordA->wrd_occ)
         return wordB->wrd_occ - wordA->wrd_occ;
+
+    // OTHERWISE, ORDER BY ALPHABETICAL ORDER
     else
         return strcmp(wordA->wrd, wordB->wrd) > 0;
 }
+
 /*
- *  Sorts and prints the output
+ * Print the sorted words and their occurencies
  *
  * Parameters:
- * words:   Array of words structs to be sorted and printed.
+ * words:   Pointer to the array of words to be printed
  *
  * Returns:
  * None
@@ -184,36 +215,39 @@ void print_sorted(word_t *words)
 {
     for (int i = 0; i < UNIQ_WORDS_COUNT; i++)
     {
-        // PRINTING THE OUTPUT
+        // PRINTING THE WORDS AND THEIR OCCURENCIES IF THE WORD IS NOT EMPTY
         if (*words[i].wrd != '\0')
         {
-            // NUMBER OF LINES A WORD CAN APPEAR ON AS A STRING
-            char word_lines[255];
+            // STRING TO STORE THE LINE NUMBERS OF THE WORD TO BE PRINTED
+            char word_lines[255] = "";
 
             // INITIALIZING TO EMPTY
             word_lines[0] = '\0';
 
             for (int k = 0; k < words[i].wrd_occ; k++)
             {
+                // STRING TEMP TO STORE THE CURRENT LINE NUMBER
                 char temp[255];
 
-                // IF LINE NUMBER EXIST, CONTINUE
+                // IF LINE NUMBER IS 0, CONTINUE
+                if (words[i].line_nbrs[k] == 0)
+                    continue;
+
+                // IF THE CURRENT LINE NUMBER IS THE SAME AS THE PREVIOUS ONE, CONTINUE
                 if (k > 0 && (words[i].line_nbrs[k] == words[i].line_nbrs[k - 1]))
                     continue;
 
+                // PRINTING THE LINE NUMBER TO THE STRING TEMP
                 snprintf(temp, sizeof(temp), k == 0 ? "%d" : ", %d", words[i].line_nbrs[k]);
 
-                // CONCATENATE CURRENT NUMBER TO THE STRING
+                // CONCATENATING THE STRING TEMP TO THE STRING word_lines
                 strcat(word_lines, temp);
             }
 
-            printf("Hello\n");
-
-            fprintf(output, "%-*s [%d]: %s\n", 15, words[i].wrd, words[i].wrd_occ, word_lines);
-            // OUTPUTTING TO THE FILE - PRINTING ERROR WHEN RETURN VALUE IS < 0
+            // PRINTING THE WORDS AND THEIR OCCURENCIES TO stdout IF THE WORD IS NOT EMPTY
             if (fprintf(stdout, "%-*s [%d]: %s\n", 15, words[i].wrd, words[i].wrd_occ, word_lines) < 0)
             {
-                fprintf(stderr, "Error writing to output: %s\n", strerror(errno));
+                fprintf(stderr, "Error writing to stdout: %s\n", strerror(errno));
                 exit(1);
             }
         }
@@ -223,35 +257,31 @@ void print_sorted(word_t *words)
 int main()
 {
     char input_line_buf[BUF_LEN];
-    char this_word[WORD_LEN];
 
     // POINTING TO THE START OF THE OF THE FIRST LINE
     char *ptr_line = input_line_buf;
 
-    output = fopen(OUTPUT_FILE, "w");
-    if (output == NULL)
-    {
-        fprintf(stderr, "Error opening output %s: %s\n", OUTPUT_FILE, strerror(errno));
-        exit(1);
-    }
-
-    // READING LINE BY LINE FROM INPUT FILE
+    // READING LINE BY LINE FROM stdin
     int line_number = 0;
     while (fgets(input_line_buf, sizeof(input_line_buf), stdin) != NULL)
     {
+        // INCREMENTING THE LINE NUMBER
+        line_number++;
+
         // CALCULATING THE LENGTH OF THE INPUT LINE
         int line_len;
-        for (line_len = 1; input_line_buf[line_len - 1] != '\0'; line_len++)
+        for (line_len = 1; input_line_buf[line_len - 1] != '\n'; line_len++)
             ;
 
-        if (line_len > 254)
+        // CHECKING IF THE LINE IS LONGER THAN 254 CHARS
+        if (line_len > BUF_LEN)
         {
             fprintf(stderr, "A line longer than 254 encountered: '%s...'\n", input_line_buf);
             exit(1);
         }
 
-        line_number++;
-        read_words(ptr_line, this_word, line_number);
+        // READING WORDS FROM THE INPUT LINE AND STORING THEM
+        read_words(ptr_line, line_number);
 
         // READ NEXT INPUT LINE. POINTING TO FRONT OF THE NEXT LINE
         ptr_line = input_line_buf;
@@ -270,18 +300,5 @@ int main()
     // PRINTING
     print_sorted(words);
 
-    if (ferror(stdin))
-    {
-        fprintf(stderr, "Error reading from stdin\n");
-        exit(1);
-    }
-
-    if (ferror(stdout))
-    {
-        fprintf(stderr, "Error writing to stdout\n");
-        exit(1);
-    }
-
-    fclose(output);
     return 0;
 }
